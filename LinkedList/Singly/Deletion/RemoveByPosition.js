3536aa9c63ff9fc509ccc2869458fd9433599408
Masuk <masukrahoman5@gmail.com>
2025-04-15 21:02:09 +0600

    Implement Remove By Position in Linked List

---
+++ LinkedList/Singly/Deletion/RemoveByPosition.js
@@ -0,0 +1,117 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  prepend(data) {
+    if (data === undefined || data === null) {
+      throw new Error("Valid Data Required");
+    }
+
+    const newNode = new Node(data);
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+
+    if (this.length === 1) {
+      this.tail = newNode;
+    }
+
+    return true;
+  }
+
+  append(data) {
+    if (data === undefined || data === null) {
+      throw new Error("Valid Data Required");
+    }
+
+    if (this.length === 0) {
+      this.prepend(data);
+      return true;
+    }
+
+    const newNode = new Node(data);
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+
+    return true;
+  }
+
+  removeFront() {
+    if (this.length === 0) {
+      throw new Error("List is Empty");
+    }
+
+    this.head = this.head.next;
+    this.length--;
+
+    return true;
+  }
+
+  removeByPosition(position) {
+    if (this.length === 0) {
+      throw new Error("List is Empty");
+    }
+    if (position < 1 || position > this.length) {
+      throw new Error("Invalid position");
+    }
+
+    if (position === 1) {
+      this.removeFront();
+      return true;
+    }
+
+    let current = this.head;
+    let prev = null;
+    let currentPosition = 1;
+
+    while (currentPosition < position) {
+      prev = current;
+      current = current.next;
+      currentPosition++;
+    }
+
+    prev.next = current.next;
+    this.length--;
+
+    if (current === this.tail) {
+      this.tail = prev;
+    }
+
+    return true;
+  }
+
+  print() {
+    let current = this.head;
+    let result = "";
+    while (current) {
+      result += `${current.data}` + `${current.next !== null ? " -> " : ""}`;
+      current = current.next;
+    }
+    return result;
+  }
+}
+
+const node = new LinkedList();
+
+node.prepend(10);
+node.prepend(20);
+node.prepend(30);
+node.prepend(40);
+
+console.log(node.print());
+
+node.removeByPosition(3);
+console.log(node.print());
