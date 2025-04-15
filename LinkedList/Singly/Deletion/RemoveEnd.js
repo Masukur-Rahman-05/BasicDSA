87189c6849e0802a9e60a92a97cee4788c900202
Masuk <masukrahoman5@gmail.com>
2025-04-15 17:29:24 +0600

    Implement Delete from end opeartion in Linked list

---
+++ LinkedList/Singly/Deletion/RemoveEnd.js
@@ -0,0 +1,97 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  prepend(data) {
+    if (data === undefined || data === null) {
+      throw new Error("Valid Data Required");
+    }
+
+    const newNode = new Node(data);
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+
+    if (this.length === 1) {
+      this.tail = newNode;
+    }
+
+    return true;
+  }
+
+  append(data) {
+    if (data === undefined || data === null) {
+      throw new Error("Valid Data Required");
+    }
+
+    if (this.length === 0) {
+      this.prepend(data);
+      return true;
+    }
+
+    const newNode = new Node(data);
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+
+    return true;
+  }
+
+  removeEnd() {
+    if (this.length === 0) {
+      throw new Error("List is Empty");
+    }
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+      this.length++;
+      return true;
+    }
+
+    let current = this.head;
+    while (current.next !== this.tail) {
+      current = current.next;
+    }
+
+    current.next = null;
+    this.tail = current;
+    this.length--;
+
+    return true;
+  }
+
+  print() {
+    let current = this.head;
+    let result = "";
+    while (current) {
+      result += `${current.data}` + `${current.next !== null ? " -> " : ""}`;
+      current = current.next;
+    }
+    return result;
+  }
+}
+
+const node = new LinkedList();
+
+node.prepend(10);
+node.prepend(20);
+node.prepend(30);
+node.prepend(40);
+
+console.log(node.print());
+
+node.removeEnd();
+console.log(node.print());
